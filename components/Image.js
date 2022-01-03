22ff4c3c0c5d8519cbe957e3500787adee4eafa4
Leo Kling <leo.kling.info@gmail.com>
2022-01-03 17:16:35 +0100

    edited site metadata, added image zooming

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,12 @@
 import NextImage from "next/image"
+import Zoom from "react-medium-image-zoom"
+import "react-medium-image-zoom/dist/styles.css"
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />
+const Image = ({ ...rest }) => (
+  <Zoom overlayBgColorEnd="rgba(20, 20, 20, 0.95)">
+    <NextImage {...rest} />
+  </Zoom>
+)
 
 export default Image
