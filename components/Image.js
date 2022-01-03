82346f2aa12d01e0f2c17232abcd3d7a9ef4836a
Leo Kling <leo.kling.info@gmail.com>
2022-01-03 17:30:56 +0100

    customized scroll bar, image zoom only for blog

--- components/Image.js
+++ components/Image.js
@@ -1,12 +1,6 @@
 import NextImage from "next/image"
-import Zoom from "react-medium-image-zoom"
-import "react-medium-image-zoom/dist/styles.css"
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => (
-  <Zoom overlayBgColorEnd="rgba(20, 20, 20, 0.95)">
-    <NextImage {...rest} />
-  </Zoom>
-)
+const Image = ({ ...rest }) => <NextImage {...rest} />
 
 export default Image
--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,11 +1,19 @@
 /* eslint-disable react/display-name */
 import { useMemo } from "react"
 import { getMDXComponent } from "mdx-bundler/client"
-import Image from "./Image"
 import CustomLink from "./Link"
 import TOCInline from "./TOCInline"
 import Pre from "./Pre"
 import { BlogNewsletterForm } from "./NewsletterForm"
+import NextImage from "next/image"
+import Zoom from "react-medium-image-zoom"
+import "react-medium-image-zoom/dist/styles.css"
+
+const Image = ({ ...rest }) => (
+  <Zoom overlayBgColorEnd="rgba(20, 20, 20, 0.95)">
+    <NextImage {...rest} />
+  </Zoom>
+)
 
 export const MDXComponents = {
   Image,
